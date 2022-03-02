2c794c33413a1f3a90b12624dbeee831336d9bb2
Tevruden <tevruden@nonexiste.net>
2022-03-02 15:58:25 -0600

    finish script

--- build.sh
+++ build.sh
@@ -2,11 +2,12 @@
 cd build
 
 
-for i in `cat ../build.txt`;  
+for i in `cat ../build.txt`; do 
 ../GALasm-master/src/galasm ../$i
-if [ $? -eq 0 ]; then 
-	result=$(echo $i | sed -e "s#/#-#")
-	cp ../$i ../builds/$result
-	git add $result
-	git commit -m "Auto add after successful build $i"
-fi
+	if [ $? -eq 0 ]; then 
+		result=$(echo $i | sed -e "s#/#-#")
+		cp ../$i ../builds/$result
+		git add $result
+		git commit -m "Auto add after successful build $i"
+	fi
+done
