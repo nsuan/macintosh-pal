b7eb7465c27d064d7321034a389ff709fbe412d9
Tevruden <tevruden@nonexiste.net>
2022-03-02 15:57:32 -0600

    add build script

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash 
+cd build
+
+
+for i in `cat ../build.txt`;  
+../GALasm-master/src/galasm ../$i
+if [ $? -eq 0 ]; then 
+	result=$(echo $i | sed -e "s#/#-#")
+	cp ../$i ../builds/$result
+	git add $result
+	git commit -m "Auto add after successful build $i"
+fi
